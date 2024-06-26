47b9dffc8401fd7e99c6e929b9a167cece2e1870
thegaab <tiagogabrielcontato@hotmail.com>
2024-06-25 22:02:50 -0300

    fix .editorconfig: eslint

--- techposts/src/server.ts
+++ techposts/src/server.ts
@@ -2,10 +2,10 @@ import { env } from './env'
 import { app } from './app'
 
 app
-    .listen({
-        host: '0.0.0.0',
-        port: env.PORT,
-    })
-    .then(() => {
-        console.log(`Let's celebrate! Server is running on http://localhost:3000`)
-    })
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`Let's celebrate! Server is running on http://localhost:3000`)
+  })
