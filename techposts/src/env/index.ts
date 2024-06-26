eee4b2bdeb986aab370f133b08dd38efb3c43af9
thegaab <tiagogabrielcontato@hotmail.com>
2024-06-25 21:30:09 -0300

    first commit

---
+++ techposts/src/env/index.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import { z } from 'zod'
+
+dotenv.config()
+
+const envSchema = z.object({
+    NODE_ENV: z
+        .enum(['development', 'production', 'test'])
+        .default('development'),
+    PORT: z.coerce.number().default(3000),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+    console.error(`Ops! Invalid environment variables`, _env.error.format())
+
+    throw new Error(`Ops! Invalid environment variables`)
+}
+
+export const env = _env.data
---
+++ techposts/src/server.ts
@@ -0,0 +1,11 @@
+import { env } from './env'
+import { app } from './app'
+
+app
+    .listen({
+        host: '0.0.0.0',
+        port: env.PORT,
+    })
+    .then(() => {
+        console.log(`Let's celebrate! Server is running on http://localhost:3000`)
+    })
